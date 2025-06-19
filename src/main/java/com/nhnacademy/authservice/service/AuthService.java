8a59e9c0a0f3fed397b003c89e31450c80aba9c4
NaIlHwan <nlhjobs01@naver.com>
2025-06-19 14:30:31 +0900

    refactor: Controller 비즈니스 로직 서비스 계층으로 이동

--- src/main/java/com/nhnacademy/authservice/service/AuthService.java
+++ src/main/java/com/nhnacademy/authservice/service/AuthService.java
@@ -1,9 +1,12 @@
 package com.nhnacademy.authservice.service;
 
+import com.nhnacademy.authservice.domain.LoginResponseDto;
 import com.nhnacademy.authservice.domain.RefreshTokenResponseDto;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public interface AuthService {
+    LoginResponseDto login(String id, String password);
+
     UserDetails authentication(String username, String password);
 
     RefreshTokenResponseDto refreshToken(String refreshToken);
