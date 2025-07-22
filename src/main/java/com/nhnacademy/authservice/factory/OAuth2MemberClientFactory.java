985d63c82e834dca2d9af164921b58dc922dc96f
NaIlHwan <nlhjobs01@naver.com>
2025-07-22 15:21:08 +0900

    feat: 유효하지 않은 provider에 대한 예외처리 추가

--- src/main/java/com/nhnacademy/authservice/factory/OAuth2MemberClientFactory.java
+++ src/main/java/com/nhnacademy/authservice/factory/OAuth2MemberClientFactory.java
@@ -1,6 +1,7 @@
 package com.nhnacademy.authservice.factory;
 
 import com.nhnacademy.authservice.client.member.OAuth2MemberClient;
+import com.nhnacademy.authservice.exception.InvalidOAuth2ProviderException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -26,6 +27,10 @@ public class OAuth2MemberClientFactory {
     }
 
     public OAuth2MemberClient getClient(String provider) {
-        return clientMap.get(provider);
+        OAuth2MemberClient client = clientMap.get(provider);
+        if (client == null) {
+            throw new InvalidOAuth2ProviderException(provider);
+        }
+        return client;
     }
 }
