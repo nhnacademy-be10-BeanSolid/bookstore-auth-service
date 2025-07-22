84b511332f6d33159e3803f9bc5a73473742570b
NaIlHwan <nlhjobs01@naver.com>
2025-07-22 15:20:11 +0900

    feat: API 명세서 관련 URL permitAll 허용 처리

--- src/main/java/com/nhnacademy/authservice/config/SecurityConfig.java
+++ src/main/java/com/nhnacademy/authservice/config/SecurityConfig.java
@@ -20,6 +20,8 @@ public class SecurityConfig {
                 .requestMatchers("/auth/**").permitAll()
                 .requestMatchers("/oauth2/**").permitAll()
                 .requestMatchers("/actuator/**").permitAll()
+                .requestMatchers("/swagger-ui/**").permitAll()
+                .requestMatchers("/v3/api-docs/**").permitAll()
                 .anyRequest().authenticated()
         );
         http.csrf(AbstractHttpConfigurer::disable);
